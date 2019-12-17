c28ec98e23726180c4a20f4f4b91054ed709c723
j-rogers <joshua.earl.rogers@gmail.com>
2019-12-17 12:07:48 +1030

    can now move lucy around using arrow keys

--- dindins/lucy.py
+++ dindins/lucy.py
@@ -8,4 +8,17 @@ class Lucy(pygame.sprite.Sprite):
         self.rect.center = (250, 100)
 
     def update(self):
-        self.rect.x += 5
+        self.speedx = 0
+        self.speedy = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -5
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 5
+        if keystate[pygame.K_UP]:
+            self.speedy = -5
+        if keystate[pygame.K_DOWN]:
+            self.speedy = 5
+
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
