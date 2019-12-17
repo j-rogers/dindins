57622bd9b9bec890d5a529d04ab70a2aa011eff5
j-rogers <j-rogers@users.noreply.github.com>
2019-12-17 19:53:31 +1030

    sprite flips when turning left/right

--- dindins/lucy.py
+++ dindins/lucy.py
@@ -2,12 +2,19 @@ import pygame
 
 from dindins.settings import *
 
+
 class Lucy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('../assets/lucy/lucy.png')
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.direction = 'right'
+
+    def _flip(self, direction):
+        if self.direction != direction:
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.direction = direction
 
     def update(self):
         self.speedx = 0
@@ -15,8 +22,10 @@ class Lucy(pygame.sprite.Sprite):
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_LEFT]:
             self.speedx = -5
+            self._flip('left')
         if keystate[pygame.K_RIGHT]:
             self.speedx = 5
+            self._flip('right')
         if keystate[pygame.K_UP]:
             self.speedy = -5
         if keystate[pygame.K_DOWN]:
