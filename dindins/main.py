72d92975c9f258e5ee5af73b827cb4e912092e8b
j-rogers <joshua.earl.rogers@gmail.com>
2019-12-18 11:14:42 +1030

    started creating main menu

--- dindins/main.py
+++ dindins/main.py
@@ -2,6 +2,7 @@ import pygame
 
 from dindins.lucy import Lucy
 from dindins.settings import *
+from dindins.gui import *
 
 
 class DinDins:
@@ -9,7 +10,7 @@ class DinDins:
         """Initilises game"""
         # Init pygame and set running to true
         pygame.init()
-        self._running = True
+        self.running = True
 
         # Build root display
         self._rootdisplay = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -17,19 +18,23 @@ class DinDins:
         # Set up clock
         self._clock = pygame.time.Clock()
 
-        # Set up initial sprites
+        # Group for sprites
         self._allsprites = pygame.sprite.Group()
+
+    def _gameinit(self):
+        # Set up initial sprites
         player = Lucy()
         self._allsprites.add(player)
 
     def _cleanup(self):
         """Cleans up and quits pygame"""
         pygame.quit()
+        exit(0)
 
     def _handle(self, event):
         """Event handler"""
         if event.type == pygame.QUIT:
-            self._running = False
+            self.running = False
 
     def _render(self):
         self._rootdisplay.fill((0,0,0))
@@ -39,9 +44,37 @@ class DinDins:
     def _update(self):
         self._allsprites.update()
 
-    def run(self):
-        """Runs the game"""
-        while self._running:
+    def run_mainmenu(self):
+        """Displays the main menu"""
+        # Title
+        title = Text('Din Dins', RED, size=50)
+        title.rect.center = (WIDTH / 2, HEIGHT / 8)
+
+        # Play button
+        play = Button('Play', RED, GREEN, BLUE, 100, 100, self._rootdisplay)
+        play.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+        while self.running:
+            # Handlers
+            self._clock.tick(FPS)
+            for event in pygame.event.get():
+                self._handle(event)
+
+            # Button functionality
+            play.update()
+
+            # Place objects on screen
+            self._rootdisplay.blit(title.text, title.rect)
+            self._rootdisplay.blit(play.text, play.rect)
+
+            # Update display
+            pygame.display.update()
+
+        #self._cleanup()
+
+    def run_game(self):
+        self._gameinit()
+        while self.running:
             self._clock.tick(FPS)
             for event in pygame.event.get():
                 self._handle(event)
@@ -50,7 +83,8 @@ class DinDins:
 
         self._cleanup()
 
-
 if __name__ == '__main__':
     game = DinDins()
-    game.run()
+    game.run_mainmenu()
+    game.running = True
+    game.run_game()
--- dindins/settings.py
+++ dindins/settings.py
@@ -1,4 +1,9 @@
-WIDTH = 500
-HEIGHT = 200
+WIDTH = 1000
+HEIGHT = 800
 FPS = 22
 ASSETS = '../assets'
+
+# Colours
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
