06557bbe2baaa90a5060d6a1ba54c5e1e87ce5ed
j-rogers <joshua.earl.rogers@gmail.com>
2019-12-18 11:39:13 +1030

    buttons can now have associated actions

--- dindins/gui.py
+++ dindins/gui.py
@@ -8,7 +8,7 @@ class Text:
         self.rect = self.text.get_rect()
 
 class Button(Text):
-    def __init__(self, text, fg, ic, ac, width, height, display, font='freesansbold.ttf', size=18):
+    def __init__(self, text, fg, ic, ac, width, height, display, font='freesansbold.ttf', size=18, action=None):
         super().__init__(text, fg, font=font, size=size)
 
         self.ic = ic
@@ -16,14 +16,22 @@ class Button(Text):
         self.width = width
         self.height = height
         self.display = display
+        self.action = action
 
     def update(self):
         mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        # Get x, y to position rect
         x, y = self.rect.center
         x -= self.width / 2
         y -= self.height / 2
+
         if x + self.width > mouse[0] > x and y + self.height > mouse[1] > y:
             pygame.draw.rect(self.display, self.ac, (x, y, self.width, self.height))
+
+            if click[0] and self.action:
+                self.action()
         else:
             pygame.draw.rect(self.display, self.ic, (x, y, self.width, self.height))
 
--- dindins/main.py
+++ dindins/main.py
@@ -21,6 +21,9 @@ class DinDins:
         # Group for sprites
         self._allsprites = pygame.sprite.Group()
 
+        # Group for buttons
+        self._buttons = []
+
     def _gameinit(self):
         # Set up initial sprites
         player = Lucy()
@@ -37,40 +40,56 @@ class DinDins:
             self.running = False
 
     def _render(self):
+        """Renders the game
+
+        Render is used to display the sprites currently on screen in the game.
+        """
         self._rootdisplay.fill((0,0,0))
         self._allsprites.draw(self._rootdisplay)
         pygame.display.flip()
 
     def _update(self):
+        """Updates all functional objects
+
+        Performs an update on all functional objects, such as (some) sprites (e.g. Lucy) and buttons.
+        """
         self._allsprites.update()
 
-    def run_mainmenu(self):
+        for button in self._buttons:
+            button.update()
+
+    def run(self):
         """Displays the main menu"""
         # Title
         title = Text('Din Dins', RED, size=50)
         title.rect.center = (WIDTH / 2, HEIGHT / 8)
 
         # Play button
-        play = Button('Play', RED, GREEN, BLUE, 100, 100, self._rootdisplay)
+        play = Button('Play', RED, GREEN, BLUE, 100, 100, self._rootdisplay,  action=self.run_game)
         play.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self._buttons.append(play)
+
+        # Options button
+        options = Button('Options', RED, GREEN, BLUE, 100, 100, self._rootdisplay)
+        options.rect.center = (WIDTH / 2, HEIGHT / 2 + 110)
+        self._buttons.append(options)
 
         while self.running:
             # Handlers
             self._clock.tick(FPS)
             for event in pygame.event.get():
                 self._handle(event)
-
-            # Button functionality
-            play.update()
+            self._update()
 
             # Place objects on screen
             self._rootdisplay.blit(title.text, title.rect)
             self._rootdisplay.blit(play.text, play.rect)
+            self._rootdisplay.blit(options.text, options.rect)
 
             # Update display
             pygame.display.update()
 
-        #self._cleanup()
+        self._cleanup()
 
     def run_game(self):
         self._gameinit()
@@ -85,6 +104,4 @@ class DinDins:
 
 if __name__ == '__main__':
     game = DinDins()
-    game.run_mainmenu()
-    game.running = True
-    game.run_game()
+    game.run()
