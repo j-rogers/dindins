a6f86b961672a79fe944705cd6799ff58e4cac71
j-rogers <joshua.earl.rogers@gmail.com>
2020-01-10 10:59:35 +1030

    replaced lucy sprite

--- dindins/lucy.py
+++ dindins/lucy.py
@@ -8,62 +8,55 @@ class Lucy(pygame.sprite.Sprite):
         """Initialises Lucy"""
         # Init sprite, set direction and location
         super().__init__()
-        self.image = pygame.image.load(f'{ASSETS}/lucy/lucy.png')
+        self.image = pygame.image.load(f'{ASSETS}/lucy/idle/lucy_idle_down.png')
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2 + 50, HEIGHT / 2)
-        self.direction = 'right'
+        self.direction = 'down'
         self.index = 0
+        self.ticker = 0
 
-        # Idle animation
-        self.idle = [
-            pygame.image.load(f'{ASSETS}/lucy/idle/idle1.png'),
-            pygame.image.load(f'{ASSETS}/lucy/idle/idle1.png'),
-            pygame.image.load(f'{ASSETS}/lucy/idle/idle2.png'),
-            pygame.image.load(f'{ASSETS}/lucy/idle/idle2.png'),
-            pygame.image.load(f'{ASSETS}/lucy/idle/idle3.png'),
-            pygame.image.load(f'{ASSETS}/lucy/idle/idle4.png')
-        ]
+        # Idle
+        self.idle = {
+            'up': pygame.image.load(f'{ASSETS}/lucy/idle/lucy_idle_up.png'),
+            'down': pygame.image.load(f'{ASSETS}/lucy/idle/lucy_idle_down.png'),
+            'left': pygame.image.load(f'{ASSETS}/lucy/idle/lucy_idle_left.png'),
+            'right': pygame.image.load(f'{ASSETS}/lucy/idle/lucy_idle_right.png')
+        }
 
         # Walking animation
-        self.walk = [
-            pygame.image.load(f'{ASSETS}/lucy/walk/walk1.png'),
-            pygame.image.load(f'{ASSETS}/lucy/walk/walk2.png'),
-            pygame.image.load(f'{ASSETS}/lucy/walk/walk3.png'),
-            pygame.image.load(f'{ASSETS}/lucy/walk/walk4.png'),
-            pygame.image.load(f'{ASSETS}/lucy/walk/walk5.png'),
-            pygame.image.load(f'{ASSETS}/lucy/walk/walk6.png')
-        ]
+        self.walk = {
+            'up': [
+                pygame.image.load(f'{ASSETS}/lucy/walk/up/lucy_walk_up_1.png'),
+                pygame.image.load(f'{ASSETS}/lucy/walk/up/lucy_walk_up_2.png')],
+            'down': [
+                pygame.image.load(f'{ASSETS}/lucy/walk/down/lucy_walk_down_1.png'),
+                pygame.image.load(f'{ASSETS}/lucy/walk/down/lucy_walk_down_2.png')],
+            'left': [
+                pygame.image.load(f'{ASSETS}/lucy/walk/left/lucy_walk_left_1.png'),
+                pygame.image.load(f'{ASSETS}/lucy/walk/left/lucy_walk_left_2.png')],
+            'right': [
+                pygame.image.load(f'{ASSETS}/lucy/walk/right/lucy_walk_right_1.png'),
+                pygame.image.load(f'{ASSETS}/lucy/walk/right/lucy_walk_right_2.png')]
+        }
 
-    def _flip(self, direction):
-        """Flips the direction of Lucy
-
-        Flips Lucy left or right if she is not already facing that way.
-
-        Args:
-            direction: String of 'left' or 'right'
-        """
-        if self.direction != direction:
-            self.image = pygame.transform.flip(self.image, True, False)
-            self.direction = direction
-
-    def _playanimation(self, animation):
+    def _playanimation(self, animation, rate=11):
         """Plays the specified animation
 
         Args:
             animation: List of images that create the animation
             index: Index that specifies what current image should be shown
         """
-        # Reset the index if reached the end of the animation
-        self.index = 0 if self.index == (len(animation) - 1) else self.index
+        # Plays 2 frames per second
+        if self.ticker % rate == 0:
+            # Reset the index if reached the end of the animation
+            self.index = 0 if self.index == len(animation) else self.index
 
-        # Flip image if we are going left
-        if self.direction == 'left':
-            self.image = pygame.transform.flip(animation[self.index], True, False)
-        else:
             self.image = animation[self.index]
 
-        # Increase index
-        self.index += 1
+            # Increase index
+            self.index += 1
+
+        self.ticker += 1
 
     def update(self):
         """Update sprite
@@ -75,19 +68,22 @@ class Lucy(pygame.sprite.Sprite):
 
         # Set animation and direction of Lucy
         if keystate[pygame.K_LEFT]:
-            self._playanimation(self.walk)
-            self._flip('left')
+            self.direction = 'left'
+            self._playanimation(self.walk[self.direction])
         if keystate[pygame.K_RIGHT]:
-            self._playanimation(self.walk)
-            self._flip('right')
+            self.direction = 'right'
+            self._playanimation(self.walk[self.direction])
         if keystate[pygame.K_UP]:
-            self._playanimation(self.walk)
+            self.direction = 'up'
+            self._playanimation(self.walk[self.direction])
         if keystate[pygame.K_DOWN]:
-            self._playanimation(self.walk)
+            self.direction = 'down'
+            self._playanimation(self.walk[self.direction])
 
         # Play idle animation when not moving
         if not keystate[pygame.K_LEFT] and not keystate[pygame.K_RIGHT] and not keystate[pygame.K_UP] and not keystate[pygame.K_DOWN]:
-            self._playanimation(self.idle)
+            self.image = self.idle[self.direction]
+            self.ticker = 0
 
 
 
